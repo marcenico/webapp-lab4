38fd9d8ee1daf0652456bce9eefd5639a0153225
marcenp <marcenp23@gmail.com>
2018-12-09 01:12:38 -0300

    Agregados campos para agregar cliente, agregado principio de validaciones

--- src/app/components/clientes/agregar-cliente/agregar-cliente.component.ts
+++ src/app/components/clientes/agregar-cliente/agregar-cliente.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Form } from '@angular/forms';
+import { Form, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar-cliente',
@@ -7,12 +7,23 @@ import { Form } from '@angular/forms';
 })
 export class AgregarClienteComponent implements OnInit {
 
+  forma: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    this.validarFormulario();
   }
 
-  public agregarCliente(forma: Form){
+  private validarFormulario() {
+    this.forma = new FormGroup({
+      'nameSU': new FormControl('', [Validators.required, Validators.minLength(3)]),
+      'passwordSU': new FormControl('', [Validators.required, Validators.minLength(8)]),
+      'emailSU': new FormControl('', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")])
+    })
+  }
+
+  public agregarCliente(forma: Form) {
 
   }
 
