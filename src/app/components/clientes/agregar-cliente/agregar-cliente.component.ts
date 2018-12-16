e577609946a0ac7a1effa25bd00eac619e8b2458
marcenp <marcenp23@gmail.com>
2018-12-16 01:57:50 -0300

    Agregado validacion y mensajes de advertencia en el formulario para agregar un nuevo cliente

--- src/app/components/clientes/agregar-cliente/agregar-cliente.component.ts
+++ src/app/components/clientes/agregar-cliente/agregar-cliente.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Form, FormControl, FormGroup, Validators } from '@angular/forms';
+import { DomicilioService } from 'src/app/services/domicilio.service';
+import { ClienteService } from 'src/app/services/clientes.service';
+import { Cliente, Domicilio } from 'src/app/shared/sdk';
+import { ClientesComponent } from '../clientes.component';
 
 @Component({
   selector: 'app-agregar-cliente',
@@ -8,11 +12,14 @@ import { Form, FormControl, FormGroup, Validators } from '@angular/forms';
 export class AgregarClienteComponent implements OnInit {
 
   forma: FormGroup;
+  private domicilio: Domicilio = new Domicilio();
 
-  constructor() { 
+  constructor(private clienteService: ClienteService) {
+    this.domicilio.cliente_domicilio = new Cliente();
+    this.domicilio.cliente_domicilio.saldo = 0;
     this.validarFormulario();
   }
-
+  
   ngOnInit() {
   }
 
@@ -23,14 +30,15 @@ export class AgregarClienteComponent implements OnInit {
       'saldo': new FormControl('', []),
       'calle': new FormControl('', [Validators.required]),
       'numero': new FormControl('', [Validators.required]),
-      'localidad': new FormControl('', [Validators.required]),   
-      'latitud': new FormControl('', [Validators.required]),   
-      'longitud': new FormControl('', [Validators.required])   
+      'localidad': new FormControl('', [Validators.required]),
+      'latitud': new FormControl('', [Validators.required]),
+      'longitud': new FormControl('', [Validators.required])
     })
   }
 
-  public agregarCliente(forma: Form) {
-
+  public agregarCliente() {
+    console.log(this.domicilio);
+    this.clienteService.create(this.domicilio);
   }
 
 }
