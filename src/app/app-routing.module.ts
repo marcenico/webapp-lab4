8b8c786101ab85442c56392e7391efc0454c18f1
marcenp <marcenp23@gmail.com>
2019-01-06 15:58:35 -0300

    Agregadas nuevas rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './components/auth/auth.component';
 import { HomeComponent } from './components/home/home.component';
 import { ClientesComponent } from './components/clientes/clientes.component';
+import { AgregarClienteComponent } from './components/clientes/agregar-cliente/agregar-cliente.component';
 
-const routes: Routes = [
+const app_routes: Routes = [
   { path: 'auth', component: AuthComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'home/clientes', component: ClientesComponent },
+  { path: 'cliente/:id', component: AgregarClienteComponent },
   { path: '**', redirectTo: 'auth', pathMatch: 'full' }
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(app_routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
